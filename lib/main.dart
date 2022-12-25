35fb88d29367a81de6010664c7b263c3d8713abb
Jesse-Soft <onyedirejesse@gmail.com>
2022-12-25 01:30:43 -1200

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  List<String> title = [
+    '+234-8070451885',
+    'onyedirejesse@gmail.com',
+    'cee_hay',
+    'https://github.com/Jesse-soft',
+    'www.linkedin.com/in/jesse-onyedire-92787825b',
+  ];
+  List<String> logo = [
+    'telephone',
+    'gmail',
+    'twitter',
+    'github',
+    'linkedin',
+  ];
+  Column getCard() {
+    List<Card> newCarditem = [];
+    for (int i = 0; i < title.length; i++) {
+      var newItem = Card(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        color: Colors.white,
+        child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListTile(
+              leading: ClipRRect(
+                child: SizedBox(
+                  height: 20,
+                  width: 20,
+                  child: Image.asset("lib/icons/${logo[i]}.png"),
+                ),
+              ),
+              title: Text(
+                title[i],
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.teal[900]),
+              ),
+            )),
+      );
+      newCarditem.add(newItem);
+    }
+    return Column(
+      children: newCarditem,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,39 +92,7 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal[400],
                 ),
               ),
-              Card(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                color: Colors.white,
-                child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal[900],
-                      ),
-                      title: Text(
-                        '+234-8070451885',
-                        style: TextStyle(fontSize: 16, color: Colors.teal[900]),
-                      ),
-                    )),
-              ),
-              Card(
-                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
-                color: Colors.white,
-                child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal[900],
-                      ),
-                      title: Text(
-                        'onyedirejesse@gmail.com',
-                        style: TextStyle(color: Colors.teal[900], fontSize: 16),
-                      ),
-                    )),
-              ),
+              getCard(),
             ],
           ),
         ),
